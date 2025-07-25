75ca70ffa26a2b6dbe3c85e6b372a21187a404a3
Eliza <elizaaguiar2015@hotmail.com>
2025-07-24 22:21:41 -0300

    day-2-changes

--- src/app/componentes/item-form/item-form.component.ts
+++ src/app/componentes/item-form/item-form.component.ts
@@ -1,38 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
 import { FormGroup, Validators, FormControl, ReactiveFormsModule, Form, AbstractControl, ValidationErrors } from '@angular/forms';
+import { NG_VALIDATORS } from '@angular/forms';
+import { forwardRef } from '@angular/core';
+import { Validator } from '@angular/forms';
 
-function validarNome(control: AbstractControl): ValidationErrors [string] {
-  const name = control.value;
-  if(name == null) {
+@Directive({
+  selector: '[inputCaracters]',
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => InputCaractersDirective),
+      multi: true
+    }
+  ]
+})
+export class InputCaractersDirective implements Validator {
+  validate(control: AbstractControl): ValidationErrors | null {
+    const valor= String(control.value);
+    const regex = '/.e/';
+    if (/regex/.test(String(control.value))){
+      return { numberInvalid: true };
+    }
     return null;
   }
-  // if (){
-  //   return {nomeInvalido: true}
-  // }
-}
-
-function validarIdade(control: AbstractControl): ValidationErrors [number] | null {
-  const valor = control.value;
-  if(!Number.isInteger(valor)){
-    return {naoInteiro: true}
-  }
-  return null;
 }
 function validarPaternidade(control: AbstractControl): ValidationErrors | null {
   const valor = control.value;
   if(valor == null) {
     return null;
   }
-  if (!(valor < 18)){
+  if (valor < 0){
     return {idadeFilho: true};
   }
   if (!(valor >= 18 && valor < 190)){
     return {idadePai: true};
   }
   if(valor.idadePai < valor.idadeFilho){
-    return {paternidade: false};
+    return {paternity: false};
   }
-
   return null;
 }
 
@@ -41,6 +47,7 @@ interface FormUser {
     email: FormControl <string>;
     idadePai: FormControl <number | null>; 
     idadeFilho: FormControl <number | null>;
+    paternity?: FormControl<boolean | null>;
 }
 
 @Component({
@@ -50,14 +57,15 @@ interface FormUser {
   styleUrl: './item-form.component.scss'
 })
 
-export class ItemFormComponent {
+export class ItemFormComponent implements ValidationErrors {
   form: FormGroup<FormUser>;
   constructor () {
     this.form = new FormGroup<FormUser> ({
       nome: new FormControl ('', { nonNullable: true, validators: [Validators.required, Validators.minLength(2), Validators.maxLength(200)] }),
       email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email]}),
-      idadePai: new FormControl (null, [Validators.required, validarIdade, validarPaternidade]),
-      idadeFilho: new FormControl (null, [Validators.required, validarIdade, validarPaternidade])
+      idadePai: new FormControl (null, [Validators.required, validarPaternidade]),
+      idadeFilho: new FormControl (null, [Validators.required, validarPaternidade]),
+      paternity: new FormControl <boolean | null> (null, [validarPaternidade])
     });
   }
   onSubmit(){
@@ -66,4 +74,5 @@ export class ItemFormComponent {
     } else { this.form.markAllAsTouched();
     } console.log('Formulário Inválido');
   }
+  
 }
